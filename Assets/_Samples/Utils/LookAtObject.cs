7c01584a21e75329ffbb7cce0af185522c02fd75
Andr�s Saraos Luna <eldruz@outlook.com>
2017-03-12 10:43:18 +0100

    Adds a smooth looking rotation.

--- Assets/_Samples/Utils/LookAtObject.cs
+++ Assets/_Samples/Utils/LookAtObject.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 
-public class LookAtObject : MonoBehaviour {
+public class LookAtObject : MonoBehaviour
+{
 
-	public Transform toFollow;
+    [SerializeField] private Transform toFollow;
+    [SerializeField] private bool isSmooth;
+    [SerializeField] private float smoothFactor = 1f;
 
-	void Update () {
-		transform.LookAt(toFollow);
-	}
+    void Update()
+    {
+        if (!isSmooth)
+        {
+            transform.LookAt(toFollow);
+        }
+        else
+        {
+            Vector3 target = toFollow.position - transform.position;
+            Quaternion rotateTo = Quaternion.LookRotation(target);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotateTo, Time.deltaTime * smoothFactor);
+        }
+    }
 }
